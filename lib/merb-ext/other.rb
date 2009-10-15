3feef8f6c1a2d2aff39a7b6cf31ecf0f3e04885e
Tymon Tobolski <i@teamon.eu>
2009-10-15 21:45:04 +0200

    Float extensions

--- lib/merb-ext/other.rb
+++ lib/merb-ext/other.rb
@@ -4,3 +4,17 @@ class String
     Iconv.iconv('ascii//translit//IGNORE', 'utf-8', self).first.gsub("'", "").gsub(/[^\x00-\x7F]+/, '').gsub(/[^a-zA-Z0-9-]+/, '-').gsub(/^-/, '').gsub(/-$/, '').downcase
   end
 end
+
+class Float
+  def round_to(x)
+    (self * 10**x).round.to_f / 10**x
+  end
+
+  def ceil_to(x)
+    (self * 10**x).ceil.to_f / 10**x
+  end
+
+  def floor_to(x)
+    (self * 10**x).floor.to_f / 10**x
+  end
+end
